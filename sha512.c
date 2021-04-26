13b98adf1cd290b371cf102ce049969262c89c3d
root <root@DESKTOP-AP321R7.localdomain>
2021-04-26 14:22:55 +0100

    Preprocessing SHA512

--- sha512.c
+++ sha512.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <inttypes.h>
+
+// Endianess. Adapted from:
+//   https://developer.ibm.com/technologies/systems/articles/au-endianc/
+#include <byteswap.h>
+const int _i = 1;
+#define islilend() ((*(char*)&_i) != 0)
  
+
 // Words and bytes.
 #define WORD uint64_t
 #define PF PRIx64
@@ -59,3 +66,147 @@ const WORD K[] = {
         0x4cc5d4becb3e42b6, 0x597f299cfc657e2a, 0x5fcb6fab3ad6faec, 0x6c44198c4a475817
 };
 
+// Returns 1 if it created a new block from original message or padding.
+// Returns 0 if all padded message has already been consumed.
+int next_block(FILE *f, union Block *M, enum Status *S, uint64_t *nobits) {
+
+    // Number of bytes read.
+    size_t nobytes;
+
+    if (*S == END) {
+        // Finish.
+        return 0;
+    } else if (*S == READ) {
+        // Try to read 128 bytes from the input file.
+        nobytes = fread(M->bytes, 1, 64, f);
+        // Calculate the total bits read so far.
+        *nobits = *nobits + (8 * nobytes);
+        // Enough room for padding.
+        if (nobytes == 128) {
+            // This happens when we can read 128 bytes from f.
+            // Do nothing.
+        } else if (nobytes < 112) {
+            // This happens when we have enough roof for all the padding.
+            // Append a 1 bit (and seven 0 bits to make a full byte).
+            M->bytes[nobytes] = 0x80; // In bits: 10000000.
+            // Append enough 0 bits, leaving 64 at the end.
+            for (nobytes++; nobytes < 112; nobytes++) {
+                M->bytes[nobytes] = 0x00; // In bits: 00000000
+            }
+            // Append nobits as a big endian integer.
+            M->sixf[15] = (islilend() ? bswap_64(*nobits) : *nobits);
+            // Say this is the last block.
+            *S = END;
+        } else {
+            // Got to the end of the input message and not enough room
+            // in this block for all padding.
+            // Append a 1 bit (and seven 0 bits to make a full byte.)
+            M->bytes[nobytes] = 0x80;
+            // Append 0 bits.
+            for (nobytes++; nobytes < 128; nobytes++) {
+                 // Error: trying to write to 
+                M->bytes[nobytes] = 0x00; // In bits: 00000000
+            }
+            // Change the status to PAD.
+            *S = PAD;
+        }
+    } else if (*S == PAD) {
+        // Append 0 bits.
+        for (nobytes = 0; nobytes < 112; nobytes++) {
+            M->bytes[nobytes] = 0x00; // In bits: 00000000
+        }
+        // Append nobits as a big endian integer.
+        M->sixf[15] = (islilend() ? bswap_64(*nobits) : *nobits);
+        // Change the status to END.
+        *S = END;
+    }
+
+    // Swap the byte order of the words if we're little endian.
+    if (islilend())
+        for (int i = 0; i < 16; i++)
+            M->words[i] = bswap_64(M->words[i]);
+
+    return 1;
+}
+
+
+int next_hash(union Block *M, WORD H[]) {
+  
+    // Message schedule, Section 6.4.2
+    WORD W[80];
+    // Iterator.
+    int t;
+    // Temporary variables.
+    WORD a, b, c, d, e, f, g, h, T1, T2;
+
+    // Section 6.4.2, part 1.
+    for (t = 0; t < 16; t++)
+        W[t] = M->words[t];
+    for (t = 16; t < 80; t++)
+        W[t] = Sig1(W[t-2]) + W[t-7] + Sig0(W[t-15]) + W[t-16];
+
+    // Section 6.4.2, part 2.
+    a = H[0]; b = H[1]; c = H[2]; d = H[3];
+    e = H[4]; f = H[5]; g = H[6]; h = H[7];
+
+    // Section 6.4.2, part 3.
+    for (t = 0; t < 80; t++) {
+        T1 = h + SIG1(e) + CH(e, f, g) + K[t] + W[t];
+        T2 = SIG0(a) + MAJ(a, b, c);
+        h = g; g = f; f = e; e = d + T1; d = c; c = b; b = a; a = T1 + T2;
+    }
+
+    // Section 6.4.2, part 4.
+    H[0] = a + H[0]; H[1] = b + H[1]; H[2] = c + H[2]; H[3] = d + H[3];
+    H[4] = e + H[4]; H[5] = f + H[5]; H[6] = g + H[6]; H[7] = h + H[7];
+
+    return 0;
+}
+
+
+int sha512(FILE *f, WORD H[]) {
+    // The function that performs/orchestrates the SHA512 algorithm on
+    // message f.
+
+    // The current block.
+    union Block M;
+
+    // Total number of bits read.
+    uint64_t nobits = 0;
+
+    // Current status of reading input.
+    enum Status S = READ;
+
+    // Loop through the (preprocessed) blocks.
+    while (next_block(f, &M, &S, &nobits)) {
+        next_hash(&M, H);
+    }
+
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    // Section 5.3.5
+    WORD H[] = {
+        0x6a09e667f3bcc908, 0xbb67ae8584caa73b, 0x3c6ef372fe94f82b, 0xa54ff53a5f1d36f1,
+        0x510e527fade682d1, 0x9b05688c2b3e6c1f, 0x1f83d9abfb41bd6b, 0x5be0cd19137e2179
+    };
+
+    // File pointer for reading.
+    FILE *f;
+    // Open file from command line for reading.
+    f = fopen(argv[1], "r");
+
+    // Calculate the SHA256 of f.
+    sha512(f, H);
+
+    // Print the final SHA256 hash.
+    for (int i = 0; i < 8; i++)
+        printf("%016" PF, H[i]);
+    printf("\n");
+
+    // Close the file.
+    fclose(f);
+
+    return 0;
+}
